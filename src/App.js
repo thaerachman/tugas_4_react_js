405aebe9e20207ae47beb353c5f3bdc3a0ef963e
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-09-29 22:44:25 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import ListMakanan from './ListData/ListMakanan'
+import MenuMakanan from './Page/MenuMakanan'
+
+function App() {
+  return (
+    <div>
+    <center>
+    <ListMakanan />
+    <MenuMakanan />
+</center>
+
+
+    </div>
+  );
+}
+
+export default App;
